35b2c2f09cfc5ff7f8717e95d7eafdb3782b2211
Gaikwad Radhika <radhika.sai.gaikwad@gmail.com>
2026-04-19 14:27:24 +0530

    added

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL || "https://34.54.116.200.nip.io/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://interview-backend-1-hdll.onrender.com/api",
   withCredentials: true, // ✅ required if you ever use cookies
 });
 
